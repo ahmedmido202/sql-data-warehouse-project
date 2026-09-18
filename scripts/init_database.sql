3e0ecc474f0ca82ca4f08517bff3e926cf05ccab
ahmedmido202 <boikaa239@gmail.com>
2026-09-18 03:34:35 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -7,4 +7,24 @@ CREATE DATABASE dw_bronze;
 CREATE DATABASE dw_silver;
 CREATE DATABASE dw_gold;
 
-USE dw_bronze;
+USE dw_bronze; 
+
+--sql server  
+
+USE master;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
