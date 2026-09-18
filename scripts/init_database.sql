eb278ee7cb95964dee52470d65db90761603fc32
ahmedmido202 <boikaa239@gmail.com>
2026-09-18 03:31:16 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS DataWarehouse;
+DROP DATABASE IF EXISTS dw_bronze;
+DROP DATABASE IF EXISTS dw_silver;
+DROP DATABASE IF EXISTS dw_gold;
+
+CREATE DATABASE dw_bronze;
+CREATE DATABASE dw_silver;
+CREATE DATABASE dw_gold;
+
+USE dw_bronze;
